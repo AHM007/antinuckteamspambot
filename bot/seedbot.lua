4b7201a8677286f362473379636f6d3522ae41c4
AHM007 <amirhosseinmoaseri92@gmail.com>
2016-08-09 03:06:02 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {257598244,133839753},--Sudo users
+    sudo_users = {257598244},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
